a1417258b38174e78d8c14765e57974b4bc2f729
Devansh-Maurya <devansh233@gmail.com>
2020-06-07 22:10:35 +0530

    Deleted some redundant code from DI modules

--- app/src/main/java/maurya/devansh/headlines/di/AppModule.kt
+++ app/src/main/java/maurya/devansh/headlines/di/AppModule.kt
@@ -1,10 +1,8 @@
 package maurya.devansh.headlines.di
 
-import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
-import maurya.devansh.headlines.HeadlinesApplication
 import maurya.devansh.headlines.screens.newslist.viewmodel.NewsListViewModelFactory
 
 /**
@@ -17,12 +15,4 @@ abstract class AppModule {
     @Binds
     internal abstract fun bindNewsFeedViewModelFactory(factory: NewsListViewModelFactory)
             : ViewModelProvider.Factory
-
-//    @Binds
-//    internal abstract fun bindNewsDataSourceFactory(factory: NewsDataSourceFactory):
-//            DataSource.Factory<Int, NewsHeadline>
-
-    @Binds
-    abstract fun bindApplication(headlinesApplication: HeadlinesApplication):
-            Context
 }
--- app/src/main/java/maurya/devansh/headlines/di/RepositoryModule.kt
+++ app/src/main/java/maurya/devansh/headlines/di/RepositoryModule.kt
@@ -1,11 +1,6 @@
 package maurya.devansh.headlines.di
 
 import dagger.Module
-import dagger.Provides
-import io.reactivex.disposables.CompositeDisposable
-import maurya.devansh.headlines.db.NewsHeadlinesDb
-import maurya.devansh.headlines.network.NewsHeadlinesApiService
-import maurya.devansh.headlines.repository.news.NewsBoundaryCallback
 
 /**
  * Created by Devansh on 6/6/20
@@ -13,17 +8,4 @@ import maurya.devansh.headlines.repository.news.NewsBoundaryCallback
 
 @Module
 class RepositoryModule {
-
-    @Provides
-    fun provideNewsBoundaryCallback(newsHeadlinesDb: NewsHeadlinesDb,
-                                    newsHeadlinesApi: NewsHeadlinesApiService,
-                                    compositeDisposable: CompositeDisposable
-    ): NewsBoundaryCallback =
-        NewsBoundaryCallback(
-            newsHeadlinesDb,
-            newsHeadlinesApi,
-            compositeDisposable
-        )
-
-
 }
