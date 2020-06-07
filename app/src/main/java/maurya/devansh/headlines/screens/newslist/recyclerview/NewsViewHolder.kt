7ec7e678d7cdb581789db0bf758f5c4a98581bca
Devansh-Maurya <devansh233@gmail.com>
2020-06-07 12:17:32 +0530

    Loading images with coil

--- app/src/main/java/maurya/devansh/headlines/screens/newslist/recyclerview/NewsViewHolder.kt
+++ app/src/main/java/maurya/devansh/headlines/screens/newslist/recyclerview/NewsViewHolder.kt
@@ -1,6 +1,7 @@
 package maurya.devansh.headlines.screens.newslist.recyclerview
 
 import android.view.View
+import coil.api.load
 import kotlinx.android.synthetic.main.item_news.view.*
 import maurya.devansh.headlines.model.NewsHeadline
 import maurya.devansh.headlines.screens.BaseViewHolder
@@ -14,6 +15,7 @@ class NewsViewHolder(itemView: View): BaseViewHolder<NewsHeadline>(itemView) {
     override fun bind(item: NewsHeadline) {
         itemView.apply {
             textViewTitle.text = item.title
+            imageViewNews.load(item.urlToImage)
         }
     }
 }
