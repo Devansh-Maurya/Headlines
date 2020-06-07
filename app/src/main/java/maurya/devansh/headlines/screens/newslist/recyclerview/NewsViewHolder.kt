e893c954700ffc334cc3311a948af7e0a86d1b66
Devansh-Maurya <devansh233@gmail.com>
2020-06-07 12:39:43 +0530

    Opening full news article in Chrome custom tab

--- app/src/main/java/maurya/devansh/headlines/screens/newslist/recyclerview/NewsViewHolder.kt
+++ app/src/main/java/maurya/devansh/headlines/screens/newslist/recyclerview/NewsViewHolder.kt
@@ -1,8 +1,12 @@
 package maurya.devansh.headlines.screens.newslist.recyclerview
 
+import android.net.Uri
 import android.view.View
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.content.ContextCompat
 import coil.api.load
 import kotlinx.android.synthetic.main.item_news.view.*
+import maurya.devansh.headlines.R
 import maurya.devansh.headlines.model.NewsHeadline
 import maurya.devansh.headlines.screens.BaseViewHolder
 
@@ -16,6 +20,22 @@ class NewsViewHolder(itemView: View): BaseViewHolder<NewsHeadline>(itemView) {
         itemView.apply {
             textViewTitle.text = item.title
             imageViewNews.load(item.urlToImage)
+
+            setOnClickListener {
+                openNewsArticle(item.url)
+            }
         }
+    }
+
+    private fun openNewsArticle(newsUrl: String) {
+        val context = itemView.context
+
+        val customTabsIntent = CustomTabsIntent.Builder().run {
+            setToolbarColor(ContextCompat.getColor(context, R.color.colorPrimary))
+            addDefaultShareMenuItem()
+            setShowTitle(true)
+            build()
+        }
+        customTabsIntent.launchUrl(context, Uri.parse(newsUrl))
     }
 }
